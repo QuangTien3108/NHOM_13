33c076da25ecf3f586b3bbeb20597a438bc02880
crushter <tuan0917538713@gmail.com>
2023-10-02 09:37:54 +0700

    update

--- ThongKeSV.py
+++ ThongKeSV.py
@@ -16,7 +16,7 @@ import matplotlib.pyplot as plt
 
 
 data=pd.read_csv("diemPython.csv")
-
+matrix=data.to_numpy()
 class SV:
     def __init__(self,data):
         self.data=data
@@ -28,14 +28,7 @@ class SV:
         return sum(self.data[check])
 
 sinh_vien=SV(data)
-"""print(sinh_vien.SV_total())
-print(sinh_vien.classfition("A"))
-print(sinh_vien.classfition("B"))
-print(sinh_vien.classfition("C"))
-print(sinh_vien.classfition("D"))
-print(sinh_vien.classfition("A+"))"""
-"""def quit_gui(text):
-    text.quit()"""
+
 
 def total():
     gui_total=Toplevel()
@@ -93,18 +86,32 @@ def graph():
     gui_graph=Toplevel()
     gui_graph.geometry("250x300")
     def show():
-         print(data["Ma_lop"])
+        seleced=var.get()
+        dict={'0':'20222FE6051001','1':'20222FE6051002','2':'20222FE6051003','3':'20222FE6051004',
+              '4':'20222FE6051005','5':'20222FE6051006','6':'20222FE6051007','7':'20222FE6051008'
+              ,'8':'20222FE6051009'}
+        types = [ "A+", 'A', 'B+', "B", 'C+', 'C', 'D+', 'D', "F", 'L1', 'L2', 'TX1', 'TX2', 'Cuoi Ki' ]
+        plt.figure(figsize = (10, 6))
+        plt.bar(types, matrix[ int(seleced) ][ 3: ])
+        plt.title(f'Rate Diem Cua Lop {dict[seleced]}')
+        plt.xlabel('Diem Dat Duoc')
+        plt.ylabel('So Luong ')
+        plt.xticks(rotation = 45)
+        plt.tight_layout()
+        plt.show()
 
-    var=
-    _1 = Radiobutton(gui_graph, text = "Lop 20222FE6051001",comman=show).pack()
-    _2 = Radiobutton(gui_graph, text = "Lop 20222FE6051002",comman=show).pack()
-    _3 = Radiobutton(gui_graph, text = "Lop 20222FE6051003",comman=show).pack()
-    _4 = Radiobutton(gui_graph, text = "Lop 20222FE6051004",comman=show).pack()
-    _5 = Radiobutton(gui_graph, text = "Lop 20222FE6051005",comman=show).pack()
-    _6 = Radiobutton(gui_graph, text = "Lop 20222FE6051006",comman=show).pack()
-    _7 = Radiobutton(gui_graph, text = "Lop 20222FE6051007",comman=show).pack()
-    _8 = Radiobutton(gui_graph, text = "Lop 20222FE6051008",comman=show).pack()
-    _9 = Radiobutton(gui_graph, text = "Lop 20222FE6051009",comman=show).pack()
+    var = StringVar()
+    _1 = Radiobutton(gui_graph, text = "Lop 20222FE6051001", variable = var, value = '0', comman = show).pack()
+    _2 = Radiobutton(gui_graph, text = "Lop 20222FE6051002", variable = var, value = '1', comman = show).pack()
+    _3 = Radiobutton(gui_graph, text = "Lop 20222FE6051003", variable = var, value = '2', comman = show).pack()
+    _4 = Radiobutton(gui_graph, text = "Lop 20222FE6051004", variable = var, value = '3', comman = show).pack()
+    _5 = Radiobutton(gui_graph, text = "Lop 20222FE6051005", variable = var, value = '4', comman = show).pack()
+    _6 = Radiobutton(gui_graph, text = "Lop 20222FE6051006", variable = var, value = '5', comman = show).pack()
+    _7 = Radiobutton(gui_graph, text = "Lop 20222FE6051007", variable = var, value = '6', comman = show).pack()
+    _8 = Radiobutton(gui_graph, text = "Lop 20222FE6051008", variable = var, value = '7', comman = show).pack()
+    _9 = Radiobutton(gui_graph, text = "Lop 20222FE6051009", variable = var, value = '8', comman = show).pack()
+
+    fin_but = Button(gui_graph, text = "QUIT", comman = gui_graph.destroy).pack()
 
 
 
@@ -115,7 +122,7 @@ gui.title("Thong Ke Sinh Vien")
 but_total=Button(gui,text="Tong So Sinh Vien Tham Gia",width=27,comman=total).pack()
 but_classfition=Button(gui,text="Phan Loai Sinh Vien Theo Diem",width=27,comman=classfi).pack()
 but_graph=Button(gui,text="Pho Diem Tung Lop",width=27,comman=graph).pack()
-
+fin_but = Button(gui, text = "QUIT", comman = gui.destroy).pack()
 
 
 
